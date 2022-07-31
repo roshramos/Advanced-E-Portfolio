9677722c9019f346b791c6a23d091036c2c05ab8
roshramos <rosh.ramos30@hotmail.com>
2022-07-31 18:13:15 +1000

    Added light and dark mode/theme

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact (event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -20,8 +33,7 @@ function contact (event) {
     })
 }
 
-let isModalOpen = false;
-function toggleModal () {
+function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
         return document.body.classList.remove("modal--open")
